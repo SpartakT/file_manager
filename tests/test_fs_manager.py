82e61e85931b06afb7cbd628b2db25de981e375b
Spartak <doct.spartak@gmail.com>
2026-03-07 06:25:25 -0800

    2

--- tests/test_fs_manager.py
+++ tests/test_fs_manager.py
@@ -6,6 +6,14 @@ from unittest.mock import patch
 from filemanager.fs_manager import copy_file, delete_path, count_files, search_files, add_creation_date, analyze_sizes , human_readable_size
 
 class TestFSManager(unittest.TestCase):
+    def setUp(self):
+        self.test_dir = tempfile.mkdtemp()
+        self.test_file = os.path.join(self.test_dir, "test.txt")
+        with open(self.test_file, "w") as f:
+            f.write("test")
+
+    def tearDown(self):
+        shutil.rmtree(self.test_dir)
 
 
 if __name__ == "__main__":
