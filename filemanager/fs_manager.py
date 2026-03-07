e008096b12ee123b75e84c8cf266c651aa34d72c
Spartak <doct.spartak@gmail.com>
2026-03-07 06:46:55 -0800

    copy

--- filemanager/fs_manager.py
+++ filemanager/fs_manager.py
@@ -2,3 +2,8 @@ import os
 import shutil
 import re
 import time
+
+def copy_file(source, destination):
+    if not os.path.exists(source):
+        raise FileNotFoundError(f"{source} not found")
+    shutil.copy(source, destination)
